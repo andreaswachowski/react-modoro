209e222cad1cfa1a42232079eed945aea3061cca
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-23 15:27:54 +0200

    feat(Splash): setup initial Splash container, including Navigator

---
+++ app/components/Splash/Splash.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+Splash.propTypes = {
+
+};
+
+export default function Splash(props) {
+  return (
+    <View>
+      <Text>
+        {'Splash'}
+      </Text>
+    </View>
+  );
+}
---
+++ app/containers/Splash/SplashContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Splash } from '~/components';
+
+export default class SplashContainer extends Component {
+  static propTypes = {}
+  state = {}
+  render() {
+    return (
+      <Splash />
+    );
+  }
+}
