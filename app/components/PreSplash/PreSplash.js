5dbd9fcbefa1f1c0c2a0f4478819f966761c2b5e
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-23 17:01:42 +0200

    feat(PreSplash): center and shrink logo

--- app/components/PreSplash/PreSplash.js
+++ app/components/PreSplash/PreSplash.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 export default class PreSplash extends Component {
   static propTypes = {}
   state = {}
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Image
+          style={styles.image}
           source={require('../../images/logo.png')} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    resizeMode: 'contain',
+    height: 300
+  }
+});
