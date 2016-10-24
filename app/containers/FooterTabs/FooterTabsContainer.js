eecf3b31d7012cd8df0408b3e3af45815823d7ed
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-24 17:10:50 +0200

    feat(FooterTabs): add dispatch setFooterTab
    Tested this by temporarily adding
      return <FooterTabsContainer navigator={navigator} />;
    
    instead of
      return <SplashContainer navigator={navigator} />;
    
    in ReactModoroNavigator.js

--- app/containers/FooterTabs/FooterTabsContainer.js
+++ app/containers/FooterTabs/FooterTabsContainer.js
@@ -1,15 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { FooterTabs } from '~/components';
+import { setFooterTab } from '~/redux/modules/activeFooterTab';
 
 class FooterTabsContainer extends Component {
   static propTypes = {
-    activeFooterTab: PropTypes.string.isRequired
+    activeFooterTab: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    navigator: PropTypes.object.isRequired
   }
   state = {}
   render() {
     return (
-      <FooterTabs activeFooterTab={this.props.activeFooterTab} />
+      <FooterTabs
+        activeFooterTab={this.props.activeFooterTab}
+        onTabSelect={(tab) => this.props.dispatch(setFooterTab(tab))} />
     );
   }
 }
