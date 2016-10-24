b73aa436ed1081340da7652de9c0dc25d8d8e1bb
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-24 16:39:19 +0200

    feat(FooterTabs): add skeleton FooterTabs

---
+++ app/containers/FooterTabs/FooterTabsContainer.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react';
+import { View, Text } from 'react-native';
+import { FooterTabs } from '~/components';
+
+export default class FooterTabsContainer extends Component {
+  static propTypes = {}
+  state = {}
+  render() {
+    return (
+      <FooterTabs />
+    );
+  }
+}
