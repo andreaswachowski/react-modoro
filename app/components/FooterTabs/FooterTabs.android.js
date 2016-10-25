6c3831575cafdb459e687dba145395cc99e56d2a
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-25 16:08:56 +0200

    feat(FooterTabs/android): add Home and Leaderboard

--- app/components/FooterTabs/FooterTabs.android.js
+++ app/components/FooterTabs/FooterTabs.android.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Text, DrawerLayoutAndroid } from 'react-native';
+import { HomeContainer, LeaderboardContainer } from '~/containers';
 
 FooterTabs.propTypes = {
-
+  activeFooterTab: PropTypes.string.isRequired,
+  navigator: PropTypes.object.isRequired,
+  onTabSelect: PropTypes.func.isRequired
 };
 
 export default function FooterTabs(props) {
@@ -11,6 +14,11 @@ export default function FooterTabs(props) {
       drawerWidth={290}
       renderNavigationView={() => (
         <Text>{'THE DRAWER'}</Text>
-      )} />
+      )} >
+      {props.activeFooterTab === 'home'
+        ? <HomeContainer navigator={props.navigator} />
+        : <LeaderboardContainer navigator={props.navigator} />}
+
+    </DrawerLayoutAndroid>
   );
 }
