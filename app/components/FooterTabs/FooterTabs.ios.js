e0e5755d0132f5f4252efb9a0a4b0d69f0612c1f
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-25 11:49:45 +0200

    feat(FooterTabs): implement content for FooterTabs

--- app/components/FooterTabs/FooterTabs.ios.js
+++ app/components/FooterTabs/FooterTabs.ios.js
@@ -1,20 +1,33 @@
-import React, {PropTypes} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, { PropTypes } from 'react';
+import { TabBarIOS, Text } from 'react-native';
+import { colors } from '~/styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 FooterTabs.propTypes = {
-
+  activeFooterTab: PropTypes.string.isRequired,
+  navigator: PropTypes.object.isRequired,
+  onTabSelect: PropTypes.func.isRequired
 };
 
 export default function FooterTabs(props) {
   return (
-    <View>
-      <Text>
-        {'FooterTabs'}
-      </Text>
-    </View>
+    <TabBarIOS tintColor={colors.active}>
+      <Icon.TabBarItem
+        iconSize={35}
+        iconName='ios-home-outline' // see Ionicons documentation
+        title='Home'
+        selected={props.activeFooterTab === 'home'}
+        onPress={() => props.onTabSelect('home')}>
+        <Text>{'Hooooooooooooome!'}</Text>
+      </Icon.TabBarItem>
+      <Icon.TabBarItem
+        iconSize={35}
+        iconName='ios-trophy-outline' // see Ionicons documentation
+        title='Leaderboard'
+        selected={props.activeFooterTab === 'leaderboard'}
+        onPress={() => props.onTabSelect('leaderboard')}>
+        <Text>{'LEADERBOARD!!'}</Text>
+      </Icon.TabBarItem>
+    </TabBarIOS>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
