0b8624700ff955d297c6ef8e45e048fd7982b6de
Andreas Wachowski <andreas.wachowski@akra.de>
2016-11-08 11:23:50 +0100

    feat(authentication): update user auth on auth change

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,4 +1,4 @@
-import { firebaseAuth, facebookProvider } from '~/config/constants';
+import { firebaseAuth, facebookProvider, ref } from '~/config/constants';
 import { AccessToken } from 'react-native-fbsdk';
 
 export function getAccessToken() {
@@ -8,4 +8,8 @@ export function getAccessToken() {
 export function authWithToken(accessToken) {
   return firebaseAuth
     .signInWithCredential(facebookProvider.credential(accessToken));
+}
+
+export function updateUser(user) {
+  return ref.child(`users/${user.uid}`).set(user);
 }
