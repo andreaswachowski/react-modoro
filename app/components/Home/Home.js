67ecbf346e0782bbfc429dd7c285a6df21584d6c
Andreas Wachowski <andreas.wachowski@akra.de>
2016-11-08 12:09:21 +0100

    feat(navbar): add gear as rightButton to navbar
    According to https://github.com/react-native-community/react-native-navbar#api,
    leftButton and rightButton must be an Object or a React Element.
    
    So we cannot just check for null and pass in nothing
    ("props.leftButton ? props.leftButton : null").
    
    Therefore the trick with optionalAttrs.

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { ReactModoroNavbar } from '~/components';
+import { ReactModoroNavbar, Gear } from '~/components';
 
 Home.propTypes = {
 
@@ -9,7 +9,9 @@ Home.propTypes = {
 export default function Home(props) {
   return (
     <View>
-      <ReactModoroNavbar title='Home' />
+      <ReactModoroNavbar
+        title='Home'
+        rightButton={<Gear onPress={() => console.log('Gear!')}/>} />
       <Text>
         {'Home'}
       </Text>
--- app/components/ReactModoroNavbar/ReactModoroNavbar.js
+++ app/components/ReactModoroNavbar/ReactModoroNavbar.js
@@ -2,11 +2,18 @@ import React, { PropTypes } from 'react';
 import NavigationBar from 'react-native-navbar';
 
 ReactModoroNavbar.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  rightButton: PropTypes.element,
+  leftButton: PropTypes.element
 };
 
 export default function ReactModoroNavbar(props) {
+  let optionalAttrs = {};
+  props.leftButton && (optionalAttrs.leftButton = React.cloneElement(props.leftButton));
+  props.rightButton && (optionalAttrs.rightButton = React.cloneElement(props.rightButton));
   return (
-    <NavigationBar title={{title: props.title}}/>
+    <NavigationBar
+      {...optionalAttrs}
+      title={{title: props.title}} />
   );
 }
