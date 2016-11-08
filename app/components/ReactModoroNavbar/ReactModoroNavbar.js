fa6f3d7973f8e601635dee8c6aae7893f2e87322
Andreas Wachowski <andreas.wachowski@akra.de>
2016-11-08 11:48:24 +0100

    feat(navbar): setup skeleton Navbar

---
+++ app/components/ReactModoroNavbar/ReactModoroNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from 'react-native-navbar';
+
+ReactModoroNavbar.propTypes = {
+
+};
+
+export default function ReactModoroNavbar(props) {
+  return (
+    <NavigationBar title={{title: 'Hello!'}}/>
+  );
+}
