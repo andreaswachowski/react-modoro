andreaswachowski/react-modoro

commit 2d805f44f526ccda7474dfb2c9995398b440c0c4

feat(FooterTabs/android): add openDrawer method
This is in preparation of a to-be-added Hamburger icon which
will open the drawer when it is pressed.

app/components/FooterTabs/FooterTabs.android.js
@@ -11,6 +11,7 @@ FooterTabs.propTypes = {
 
 export default function FooterTabs(props) {
   const closeDrawer = () => this.drawer.closeDrawer();
+  const openDrawer = () => this.drawer.closeDrawer();
 
   return (
     <DrawerLayoutAndroid
@@ -23,8 +24,8 @@ export default function FooterTabs(props) {
         onTabSelect={props.onTabSelect} />
       )} >
       {props.activeFooterTab === 'home'
-        ? <HomeContainer navigator={props.navigator} />
-        : <LeaderboardContainer navigator={props.navigator} />}
+        ? <HomeContainer openDrawer={openDrawer} navigator={props.navigator} />
+        : <LeaderboardContainer openDrawer={openDrawer} navigator={props.navigator} />}
 
     </DrawerLayoutAndroid>
   );

app/containers/Home/HomeContainer.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Home } from '~/components';
 
 export default class HomeContainer extends Component {
-  static propTypes = {}
+  static propTypes = {
+    openDrawer: PropTypes.func, // not required, because not required on iOS
+    navigator: PropTypes.object.isRequired
+  }
   state = {}
   render() {
     return (

app/containers/Leaderboard/LeaderboardContainer.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Leaderboard } from '~/components';
 
 export default class LeaderboardContainer extends Component {
-  static propTypes = {}
+  static propTypes = {
+    openDrawer: PropTypes.func, // not required, because not required on iOS
+    navigator: PropTypes.object.isRequired
+  }
   state = {}
   render() {
     return (

commit c943ad5cf514d5b8befcfaf24e22f74b99758757

fix(FooterTabs/android): fix openDrawer method (bug introduced in 2d805f4)

app/components/FooterTabs/FooterTabs.android.js
@@ -11,7 +11,7 @@ FooterTabs.propTypes = {
 
 export default function FooterTabs(props) {
   const closeDrawer = () => this.drawer.closeDrawer();
-  const openDrawer = () => this.drawer.closeDrawer();
+  const openDrawer = () => this.drawer.openDrawer();
 
   return (
     <DrawerLayoutAndroid