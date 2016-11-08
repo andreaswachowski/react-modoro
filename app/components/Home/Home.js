c42986009a08930b585e4213053cee3dd76b9640
Andreas Wachowski <andreas.wachowski@akra.de>
2016-11-08 16:03:22 +0100

    feat(Home): pass openDrawer prop to Hamburger onPress

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { View, Text, Platform } from 'react-native';
 import { ReactModoroNavbar, Gear, Hamburger } from '~/components';
 
 Home.propTypes = {
-
+  openDrawer: PropTypes.func // only needed for Android
 };
 
 export default function Home(props) {
@@ -11,7 +11,7 @@ export default function Home(props) {
     <View>
       <ReactModoroNavbar
         title='Home'
-        leftButton={Platform.OS === 'android' ? <Hamburger /> : null}
+        leftButton={Platform.OS === 'android' ? <Hamburger onPress={props.openDrawer} /> : null}
         rightButton={<Gear onPress={() => console.log('Gear!')}/>} />
       <Text>
         {'Home'}
